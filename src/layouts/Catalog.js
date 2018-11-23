e938156cd233adef5aeac7db00801f24e1f8e248
Paul White <theflashofsonic@gmail.com>
2018-11-23 23:48:40 +0300

    catalog list component

--- src/layouts/Catalog.js
+++ src/layouts/Catalog.js
@@ -1,17 +1,62 @@
 import React, {Component} from 'react';
 
-//import '../css/styles.less';
+import '../css/styles.css';
 
 class Catalog extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+      super(props);
+      this.state = {};
+  }
 
-        this.state = {};
-    }
-
-    render = () => {
-        return <div>Catalog Component</div>;
-    }
+  render = () => {
+    return (
+    <div className="catalog">
+      <div className="catalog-wrapper">
+        <div className="bg"></div>
+        <div className="bg"></div>
+        <div className="bg"></div>
+        <div className="list">
+          <div className="list-wrapper tile">
+            <div className="el header">
+              <div className="id"></div>
+              <div className="name">
+                <div></div>
+              </div>
+              <div className="price">
+                <div></div>
+              </div>
+              <div className="button">
+              </div>
+            </div>
+            <div className="el tile">
+              <div className="id">1</div>
+              <div className="name">Project1</div>
+              <div className="price">43</div>
+              <div className="button">
+                <div></div>
+              </div>
+            </div>
+            <div className="el tile">
+              <div className="id">1</div>
+              <div className="name">Project1</div>
+              <div className="price">43</div>
+              <div className="button">
+              <div></div>
+              </div>
+            </div>
+            <div className="el tile">
+              <div className="id">1</div>
+              <div className="name">Project1</div>
+              <div className="price">43</div>
+              <div className="button">
+              <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>);
+  }
 }
 
 export default Catalog;
