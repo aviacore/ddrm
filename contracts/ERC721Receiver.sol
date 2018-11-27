ecbf6f9d71b27401e22b1a3fc34794f2dcd3dca6
rjkz808 <38258624+rjkz808@users.noreply.github.com>
2018-11-27 18:50:30 +0300

    refactor

--- contracts/ERC721Receiver.sol
+++ contracts/ERC721Receiver.sol
@@ -1,17 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Holder.sol";
 
-contract ERC721Receiver is IERC721Receiver {
-  function onERC721Received(
-    address operator,
-    address from,
-    uint256 tokenId,
-    bytes data
-  )
-    public
-    returns (bytes4)
-  {
-    return this.onERC721Received.selector;
-  }
+contract ERC721Receiver is ERC721Holder {
 }
